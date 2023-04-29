291a5dd03be04df31f9934e901df50237d6fc2d0
vermont42 <vermontcoder@gmail.com>
2023-04-29 15:41:34 -0700

    improve formatting

--- CatFancyTests/Helpers/NSLayoutConstraintExtensionTests.swift
+++ CatFancyTests/Helpers/NSLayoutConstraintExtensionTests.swift
@@ -13,7 +13,15 @@ class NSLayoutConstraintExtensionTests: XCTestCase {
       parentView.addSubview($0)
     }
 
-    let constraint = NSLayoutConstraint(item: view1, attribute: .bottom, relatedBy: .equal, toItem: view2, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+    let constraint = NSLayoutConstraint(
+      item: view1,
+      attribute: .bottom,
+      relatedBy: .equal,
+      toItem: view2,
+      attribute: .bottom,
+      multiplier: 1.0,
+      constant: 0.0
+    )
 
     XCTAssertFalse(constraint.isActive)
     constraint.activate()
